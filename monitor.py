dd5cc86736f1f74482366a92c31e9c68f8148d77
linzhenhan123 <916166923@qq.com>
2026-07-02 15:19:45 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,8 +1,14 @@
+import notify
+
 def main():
     print("=== monitor start ===")
 
     notify.send_push(
         "Alpha123测试通知",
-        "如果你收到说明PushPlus完全正常"
+        "如果你收到说明PushPlus正常"
     )
-print("HELLO MONITOR START")
+
+    print("END")
+
+if __name__ == "__main__":
+    main()
